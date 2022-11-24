d8c71fc40c6c23afebc2395a42a2691cf3d3c561
JigarPandya <jigarpandya.ce@gmail.com>
2022-11-24 14:12:40 +0530

    Maven based java application with MANIFEST.MF main-class entry. Supporting to be able to run as java -jar

---
+++ java/MavenDemoJavaApplication/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World of Java.");
+		System.out.println("Demonstrating simple maven build...");
+	}
+}
