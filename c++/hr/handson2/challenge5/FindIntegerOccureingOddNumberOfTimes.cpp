b50a00c3c51cf3689cadfab858ccfba5842588e0
JigarPandya <jigarpandya.ce@gmail.com>
2023-03-02 15:26:57 +0530

    Another logic using xor operator

--- c++/hr/handson2/challenge5/FindIntegerOccureingOddNumberOfTimes.cpp
+++ c++/hr/handson2/challenge5/FindIntegerOccureingOddNumberOfTimes.cpp
@@ -10,36 +10,15 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int numberOfInputs;
     int input;
-    int i,j;
-    bool isErased=false;
-    vector<int> recordOdds;
-    
+    int answer=0;
+    int i;
     cin>>numberOfInputs;
     for(i=0;i<numberOfInputs;i++)
     {
         cin>>input;
-        int size = recordOdds.size();
-        for(j=0;j<size;j++)
-        {
-			
-            if(recordOdds[j]==input)
-            {
-		//cout<<"At"<<recordOdds.at(j);
-                recordOdds.erase(recordOdds.begin()+j);
-		//cout<<"repeat:"<<recordOdds[j]<<endl;
-                isErased=true;
-                break;
-            }
-        }
-        if(isErased==false)
-        {
-            recordOdds.push_back(input);
-        }      
-        else
-            isErased=false; //next turn reset
-    }
- 
-    cout<<recordOdds.at(0);
+        answer=answer^input;
+    }       
+    cout<<answer;
     
     return 0;
 }
