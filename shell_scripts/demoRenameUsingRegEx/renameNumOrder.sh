3d947120ef6028d891f228caf5653d91a257ac8d
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-07-07 11:55:16 +0530

    Added another script for further step where the output folders will have prefix 0 to sort them by name numeric wise 1.11. vs 1.2.3.

---
+++ shell_scripts/demoRenameUsingRegEx/renameNumOrder.sh
@@ -0,0 +1,12 @@
+for fn in `ls outputFolders/.`; 
+do 
+	#any folder to exclude add entry here -o means logical or condition
+	if [ "$fn" == "LectureTakenDetails_DAA_2020_21" -o "$fn" == "JMP-O.pdf" -o "$fn" == "rename.sh" -o "$fn" == "graph" -o "$fn" == "-" ];
+	then
+		continue
+	fi
+	nfn=`echo $fn | sed 's/^\([[:alpha:]]*\)\([[:digit:]]\)$/\10\2/g'`
+	mkdir -p outputNumSortedFolders/$nfn
+done;
+
+
