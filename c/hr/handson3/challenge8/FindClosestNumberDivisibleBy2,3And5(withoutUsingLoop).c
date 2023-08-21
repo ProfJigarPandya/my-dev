e50dcd7a0e31d2cf6ddfac8b4fe7346cb5e4c6d9
ProfJigarPandya <jpandya@JigarPandyaHPL.localdomain>
2023-08-21 22:56:28 +0530

    A simple but interesting problem - finding closest smaller number

--- c/hr/handson3/challenge8/FindClosestNumberDivisibleBy2,3And5(withoutUsingLoop).c
+++ c/hr/handson3/challenge8/FindClosestNumberDivisibleBy2,3And5(withoutUsingLoop).c
@@ -1,14 +1,32 @@
 //Find closest number divisible by 2, 3 and 5 (without using loop)
 //For a given number ﬁnd the closest number divisible by 2, 3 and 5. Closest number could be smaller or greater than a given number. If there are two numbers which are divisible by 2, 3 and 5 and are at same distance from a given number, then output smaller number.
 #include <stdio.h>
-
+#define TRIO 2*3*5
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
 	int givenNumber;
 	scanf("%d",&givenNumber);
 
-
+	int leftNumber, rightNumber;
+	leftNumber = givenNumber/(TRIO) * (TRIO);
+	rightNumber= leftNumber + (TRIO);
+	//printf("%d %d %d",leftNumber, givenNumber, rightNumber);
+	int leftDistance, rightDistance;
+	leftDistance = givenNumber-leftNumber;
+	rightDistance = rightNumber-givenNumber;
+	if(leftDistance==rightDistance)
+	{
+		printf("%d",leftNumber);//print smaller
+	}
+	else if(leftDistance<rightDistance)
+	{
+		printf("%d",leftNumber);//left is nearer
+	}
+	else
+	{
+		printf("%d",rightNumber);//right is nearer
+	}
 
 
 	
