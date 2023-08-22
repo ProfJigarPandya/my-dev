388185f7e44b2ead5ccb5628dbbfcff23b46e4ca
ProfJigarPandya <jpandya@JigarPandyaHPL.localdomain>
2023-08-22 23:23:21 +0530

    dec2bin incorrect solution documented

--- c/misc/dectobin.c
+++ c/misc/dectobin.c
@@ -1,18 +1,15 @@
 /* convert decimal to binary... */
-
+//This output needs to read from right to left. This is not a true solution for decimal to binary.
 #include <stdio.h>
-#include <conio.h>
 
 
 void main()
 {
- int x=45;
- clrscr();
+ int x=16;
 
  while(x>=1)
  {
 	printf("%d",x%2);
 	x=x/2;
  }
- getch();
 }
