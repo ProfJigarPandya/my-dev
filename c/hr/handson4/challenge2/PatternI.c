069c25e74cd2550df6adb0f247baa4fbca398e97
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-08-22 14:54:52 +0530

    Handson4 challenge{1..5} and g++ added with -lm to be able to compile maths library

--- c/hr/handson4/challenge2/PatternI.c
+++ c/hr/handson4/challenge2/PatternI.c
@@ -1,7 +1,33 @@
+/*
+ 
+ Pattern I
+e.g. for n = 4 following pattern should be printed
+*
+**
+***
+****
+ 
+ * */
 #include <stdio.h>
 
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	int n,r,c;
+	scanf("%d",&n);
+	for(r=1;r<n;r++)
+	{
+		for(c=1;c<=r;c++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	//Print last row specially as we don't want newline afterwards.
+	for(c=1;c<=r;c++)
+	{
+		printf("*");
+	}
     return 0;
 }
--- c/hr/handson4/challenge3/PatternII.c
+++ c/hr/handson4/challenge3/PatternII.c
@@ -1,7 +1,30 @@
+/*Pattern I
+e.g. for n = 4 following pattern should be printed
+0123
+ 456
+  78
+   9
+*/
 #include <stdio.h>
 
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	int n,r,c;
+	scanf("%d",&n);
+	int data=0;
+	for(r=1;r<=n;r++)
+	{
+		for(c=1;c<r;c++)
+		{
+			printf(" ");
+		}
+		for(c=1;c<=(n-r+1);c++)
+		{
+			printf("%d",data++);
+		}
+		if(r<n)//No need to print a newline after the last line printed.
+			printf("\n");
+	}
     return 0;
 }
