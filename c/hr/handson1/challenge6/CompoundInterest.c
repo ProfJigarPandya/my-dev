5ff0e0ef8d5b252d6568bcc85f09a77e6bc57619
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-08-26 12:06:27 +0530

    If needed to match initial few values after decimal point, considering double arithmetic seems to be working because of high memory component and precision. Assuming that imidiate digits after decimal points are not rounded as compare to float having less storage.

--- c/hr/handson1/challenge6/CompoundInterest.c
+++ c/hr/handson1/challenge6/CompoundInterest.c
@@ -7,14 +7,13 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
 	int principalAmount;
-	float annualRateOfInterest;
-	const int durationOfLoan=3;
+	double annualRateOfInterest;
 
 	scanf("%d",&principalAmount);
-	scanf("%f",&annualRateOfInterest);
+	scanf("%lf",&annualRateOfInterest);
 
-	float interestAmount;
-	float compoundInterestAmount=0.0;
+	double interestAmount;
+	double compoundInterestAmount=0.0;
 	
 	//Compound interest step wise
 	//Year 1
@@ -31,7 +30,7 @@ int main() {
 
 	//Note that above can be better way done using loop construct. 
 
-	printf("%9.6f\n",compoundInterestAmount);
+	printf("%9.6f",compoundInterestAmount);
 
     return 0;
 }
