f8285d9a75a034fb8825558f1ec377fd830c236a
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-08-29 11:09:29 +0530

    Sum And Average Of N Numbers with output issue of \r\n

--- c/hr/handson4/challenge10/SumAndAverageOfNNumbers.c
+++ c/hr/handson4/challenge10/SumAndAverageOfNNumbers.c
@@ -1,3 +1,6 @@
+/* Sum and Average of 'n' numbers
+ *
+ * */
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -6,6 +9,22 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	int n;
+	int inputNumber;
+	int sum=0;
+	scanf("%d",&n);
+	int times=n;
+	while(times)
+	{
+		scanf("%d",&inputNumber);
+		sum+=inputNumber;
+		times--;
+	}
+	printf("%d\n",sum);
+	printf("%.2f\n",sum/(float)n);
+
+
+	
     return 0;
 }
 
