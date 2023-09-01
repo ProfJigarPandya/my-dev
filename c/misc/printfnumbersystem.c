e27d194dcc5b2a20cf93fb65059ae747fbcf598a
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-09-01 13:52:16 +0530

    a fix for handling 0 itself as data input and output. Its not preceding.

--- c/misc/printfnumbersystem.c
+++ c/misc/printfnumbersystem.c
@@ -6,22 +6,28 @@ int main()
 	unsigned int data=5024;
 	long int mask;
 	int msb,bit;
-	enum boolean flagShowZeroes=false;//do not show zeroes
+	enum boolean flagShowZeroes;
 	int totalBytes=sizeof(unsigned int);
 	printf("Decimal %d\n",data);
 	printf("Hex %x\n",data);
 	printf("Octal %o\n",data);
 	printf("Binary ");
-	for(msb=1;msb<=totalBytes*8;msb++)
+	if(data==0)
+		printf("0");
+	else
 	{
-		mask=(unsigned int)pow(2.0,totalBytes*8-msb);
-		bit=data&mask;
-		if(bit)
-		{
-			printf("1");
-			flagShowZeroes=true;
+		flagShowZeroes=false;//do not show zeroes
+		for(msb=1;msb<=totalBytes*8;msb++)	
+		{	
+			mask=(unsigned int)pow(2.0,totalBytes*8-msb);
+			bit=data&mask;
+			if(bit)
+			{
+				printf("1");
+				flagShowZeroes=true;
+			}
+			else if(flagShowZeroes)
+				printf("0");
 		}
-		else if(flagShowZeroes)
-			printf("0");
 	}
 }
