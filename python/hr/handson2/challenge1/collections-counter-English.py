cbf587ea04abb72154d8bf1948d4168bfa545a5a
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-08 12:27:05 +0530

    handson2 templates only

---
+++ python/hr/handson2/challenge1/collections-counter-English.py
@@ -0,0 +1,18 @@
+#https://www.hackerrank.com/challenges/collections-counter
+
+from collections import Counter
+
+X_num_of_shoes = int(input())
+print(X_num_of_shoes)
+
+shoe_sizes_available = list(input())
+print(shoe_sizes_available)
+
+N_num_of_customers = int(input())
+print(N_num_of_customers)
+
+for i in range(N_num_of_customers):
+    pref_size, amount = input()
+
+
+
