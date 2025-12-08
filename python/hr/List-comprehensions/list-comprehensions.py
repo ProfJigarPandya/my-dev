3848f2f47b3b1c48cbf98eed890e8f76026487ac
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-08 14:29:54 +0530

    regular vesion of solution without using list comprehension

---
+++ python/hr/List-comprehensions/list-comprehensions.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    listoflists = []
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k!=n:
+                    #print(i,j,k)
+                    listoflists.append(list([i,j,k]))
+    print(listoflists)
