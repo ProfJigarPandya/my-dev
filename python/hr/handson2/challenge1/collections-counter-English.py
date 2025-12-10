ed23f24c95ef97317a2a18cad157db4f06a59104
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-10 15:59:58 +0530

    use of tuple and collection

--- python/hr/handson2/challenge1/collections-counter-English.py
+++ python/hr/handson2/challenge1/collections-counter-English.py
@@ -2,17 +2,40 @@
 
 from collections import Counter
 
-X_num_of_shoes = int(input())
-print(X_num_of_shoes)
 
-shoe_sizes_available = list(input())
-print(shoe_sizes_available)
+X_num_of_shoes = int(input())
+
+'''
+shoe_sizes_available = []
+elements = input()
+shoe_sizes_available=[int(element) for element in elements if element != ' ']
+'''
+
+shoe_sizes_available = list(map(int, input().split()))
+
 
 N_num_of_customers = int(input())
-print(N_num_of_customers)
-
+customer_orders = []
 for i in range(N_num_of_customers):
-    pref_size, amount = input()
+    list_of_strings = input().split()
+    customer_orders.append(tuple(int(x) for x in list_of_strings))
 
 
+'''
+print("Shoe sizes available {}".format(shoe_sizes_available))
+for c in customer_orders:
+    c_size = c[0]
+    c_price = c[1]
+    print("Customer Requests Size {} and Price {}".format(c_size,c_price))
+'''
 
+total_money_earned = 0
+collection_counter = Counter(shoe_sizes_available)
+for c in customer_orders:
+    c_size = c[0]
+    c_price = c[1]
+    if collection_counter[c_size] > 0:
+        total_money_earned += c_price
+        collection_counter[c_size] -= 1
+
+print(total_money_earned,end='')
---
+++ python/hr/handson2/challenge1/readme.txt
@@ -0,0 +1,1 @@
+Here, customers are served first come first serve. No need to apply min/max optimization on pricing or profit.
