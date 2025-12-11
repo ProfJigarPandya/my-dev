0ce8808b47f27e862bbf95da8e0536016cdb87c7
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-11 14:21:14 +0530

    use of multi sort feature

--- python/hr/handson2/challenge2/most-commons-English.py
+++ python/hr/handson2/challenge2/most-commons-English.py
@@ -1,1 +1,16 @@
 #https://www.hackerrank.com/challenges/most-commons
+#https://docs.python.org/3/howto/sorting.html
+#!/bin/python3
+
+from collections import Counter
+
+
+
+s = input()
+c = Counter(s)
+most_common = c.most_common()
+most_common.sort(key=lambda x: (-x[1], x[0]))
+for a,b in most_common[0:2]:
+    print(a,b)
+a,b=most_common[2]
+print(a,b,end='')
