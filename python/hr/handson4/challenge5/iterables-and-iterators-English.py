d31a0b3bea2a3a975382c787eae655672378d859
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-22 03:53:08 +0530

    use of combination itertools

---
+++ python/hr/handson4/challenge5/iterables-and-iterators-English.py
@@ -0,0 +1,24 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations
+length_of_list_N=input()
+#print(length_of_list_N)
+
+english_letters = input().split()
+#for el in english_letters:
+#    print(el)
+    
+number_of_indices_K=int(input().strip())
+#print(number_of_indices_K)
+
+count_of_a = 0
+total_pairs=0
+comb=combinations(english_letters,number_of_indices_K)
+
+for c in comb:
+#    print(c)
+    if(c.__contains__('a')):
+        count_of_a=count_of_a+1
+    total_pairs=total_pairs+1
+#print(count_of_a)
+#print(total_pairs)
+print(count_of_a/total_pairs)
