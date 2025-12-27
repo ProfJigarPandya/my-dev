0964f205dacd1d3a98d9495d92452010c782b7e5
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-27 12:08:17 +0530

    maximize it with iterative logic

--- python/hr/handson2/challenge3/maximize-it-English.py
+++ python/hr/handson2/challenge3/maximize-it-English.py
@@ -1,35 +1,61 @@
 #https://www.hackerrank.com/challenges/maximize-it
 line1=list(map(int, input().split()))
-K=line1[0]
-M=line1[1]
-#print(K)
-#print(M)
-
+number_of_lines_K=line1[0]
+modulo_M=line1[1]
+#print(number_of_lines_K)   
+#print(module_M)
+if(number_of_lines_K==0):
+    print(0)
+    exit()
 
 lists = []
-for l in range(K):
+for l in range(number_of_lines_K):
     lists.append([])
     line = list(map(int, input().split()))
     for element in range(1,line[0]+1):
         lists[l].append(line[element])
-
 #print(lists)
-for l in range(K):
-    lists[l].sort(reverse=True)
 
-max_sum=0
+if(number_of_lines_K==1):
+    line = lists[0]
+    maximized_result=(line[0]*line[0])%modulo_M   
+    for prospective_element in line[1:]:
+        if((prospective_element*prospective_element)%modulo_M>maximized_result):
+            maximized_result = (prospective_element*prospective_element)%modulo_M
+    print(maximized_result)
+    exit()          
 
-for l in range(K):
-        max_sum+=lists[l][0]**2
+list_of_combinations_prev=[]
+for e in lists[0]:
+    list_of_combinations_prev.append([e])
 
-if max_sum%M!=0:
-    print(max_sum%M)
-elif K==1:
-    max_sum=lists[0][0]**2%M;
-    for i in range(1,len(lists[0])):
-        if lists[0][i]**2%M>max_sum:
-            max_sum=lists[0][i]**2%M
-    print(max_sum)
-else:
-    pass
-    
+for lineno in range(1,number_of_lines_K):
+    #print("lineno:",lineno)
+    list_of_combinations_current=[]
+    for sublist_locp in list_of_combinations_prev:   
+        #print("sublist_locp:",sublist_locp)
+        for line_data in lists[lineno]:        
+            #print("line_data:",line_data)
+            list_of_combinations_current.append(sublist_locp+[line_data])
+    #print("list_of_combinations_current:",list_of_combinations_current)
+    list_of_combinations_prev=list_of_combinations_current
+
+maximized_sum_of_squares=0    
+maximized_result=0
+for element in list_of_combinations_current[0]:
+    maximized_sum_of_squares+=(element*element)
+maximized_result = maximized_sum_of_squares%modulo_M
+
+#print(prospective_list)            
+prospective_sum_of_squares=0
+prospective_result=0
+
+for prospective_list in list_of_combinations_current[1:]:
+    prospective_sum_of_squares=0
+    for element in prospective_list:
+        prospective_sum_of_squares+=(element*element)
+    prospective_result=prospective_sum_of_squares%modulo_M
+    if prospective_result>maximized_result:
+        maximized_result=prospective_result
+
+print(maximized_result)
