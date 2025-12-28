e59c0dbf4e26f563e2698a371526292e0b7146e9
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-12-28 20:25:02 +0530

    learning Regular expression using python

---
+++ python/hr/handson3/challenge1/validate-list-of-email-address-with-filter-English.py
@@ -0,0 +1,23 @@
+import re
+def fun(s):
+    # return True if s is a valid email, else return False
+    try:
+        result=re.search(r'^[a-zA-Z0-9_-]+[@][a-zA-Z0-9]+[.][a-zA-Z]{1,3}$',s)
+        if result.group(0) == s:
+            return True
+    except AttributeError:
+        return False
+  
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails,end='')
